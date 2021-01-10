26af84b0854d5cfe519f9485b7893525a7ec512e
stihova <39700980+stihova@users.noreply.github.com>
2021-01-10 14:15:09 +0200

    Added Paragraph

---
+++ web/sites/default/files/php/twig/5ff9cc75a39a7_paragraph.html.twig_oG2VWrd0t219b2KepGpOXp5T2/VU1rjPb1vUwj2IMAIo2IiNHyrhu-pbL4YbLjA36R-BY.php
@@ -0,0 +1,117 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/contrib/paragraphs/templates/paragraph.html.twig */
+class __TwigTemplate_eba8037d9c50fcafb365c3b51a7e177e7c614a9c869cc706da074e51671e7d1c extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+            'paragraph' => [$this, 'block_paragraph'],
+            'content' => [$this, 'block_content'],
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("set" => 42, "block" => 49);
+        $filters = array("clean_class" => 44, "escape" => 50);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['set', 'block'],
+                ['clean_class', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 42
+        $context["classes"] = [0 => "paragraph", 1 => ("paragraph--type--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source,         // line 44
+($context["paragraph"] ?? null), "bundle", [], "any", false, false, true, 44), 44, $this->source))), 2 => ((        // line 45
+($context["view_mode"] ?? null)) ? (("paragraph--view-mode--" . \Drupal\Component\Utility\Html::getClass($this->sandbox->ensureToStringAllowed(($context["view_mode"] ?? null), 45, $this->source)))) : ("")), 3 => (( !twig_get_attribute($this->env, $this->source,         // line 46
+($context["paragraph"] ?? null), "isPublished", [], "method", false, false, true, 46)) ? ("paragraph--unpublished") : (""))];
+        // line 49
+        $this->displayBlock('paragraph', $context, $blocks);
+    }
+
+    public function block_paragraph($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 50
+        echo "  <div";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, ($context["attributes"] ?? null), "addClass", [0 => ($context["classes"] ?? null)], "method", false, false, true, 50), 50, $this->source), "html", null, true);
+        echo ">
+    ";
+        // line 51
+        $this->displayBlock('content', $context, $blocks);
+        // line 54
+        echo "  </div>
+";
+    }
+
+    // line 51
+    public function block_content($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 52
+        echo "      ";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["content"] ?? null), 52, $this->source), "html", null, true);
+        echo "
+    ";
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/paragraphs/templates/paragraph.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  92 => 52,  88 => 51,  83 => 54,  81 => 51,  76 => 50,  69 => 49,  67 => 46,  66 => 45,  65 => 44,  64 => 42,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/contrib/paragraphs/templates/paragraph.html.twig", "C:\\xampp\\htdocs\\project\\web\\modules\\contrib\\paragraphs\\templates\\paragraph.html.twig");
+    }
+}
